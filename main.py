e68e6b3316d7d277ed28913789dd50e74aa77019
Ivan Ermacoff <jermacoff@gmail.com>
2022-05-24 22:55:56 +0000

    Switch to curses

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 import sys
+from curses import wrapper
 from src import dice_roll, mad_libs_gen, rnd_password_gen, rock_paper_scissors
 
 
-def main():
+def main(stdcr):
     while True:
-        print("Python Projects")
-        print(
+        stdcr.clear()
+        stdcr.addstr("Python Projects\n")
+        stdcr.addstr(
             """
 0. Exit
 1. Dice Roll
 2. Mad Libs Generator
 3. Random Password Generator
-4. Rock Paper Scissors"""
+4. Rock Paper Scissors
+"""
         )
-        print("Your selection: ", end="")
-        match int(input()):
-            case 0:
+        stdcr.refresh()
+        match stdcr.getkey():
+            case "0":
                 break
-            case 1:
-                sys.exit(dice_roll.main())
-            case 2:
-                sys.exit(mad_libs_gen.main())
-            case 3:
-                sys.exit(rnd_password_gen.main())
-            case 4:
-                sys.exit(rock_paper_scissors.main())
+            case "1":
+                dice_roll.main()
+            case "2":
+                mad_libs_gen.main()
+            case "3":
+                rnd_password_gen.main()
+            case "4":
+                rock_paper_scissors.main()
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(wrapper(main))
