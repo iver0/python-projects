62bc6af369e559e1012c01a7f846c51f292c4288
Ivan Yermakov <jermacoff@gmail.com>
2022-05-26 14:50:13 +0000

    Remove pynput

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 import sys
-from pynput.keyboard import Key, Listener
 from src import dice_roll, mad_libs_gen, rnd_password_gen, rock_paper_scissors
-
-
-def check_key(key):
-    match key:
-        case Key.5:
-            exit
-    # keyboard.add_hotkey("1", dice_roll.main)
-    # keyboard.add_hotkey("2", mad_libs_gen.main)
-    # keyboard.add_hotkey("3", rnd_password_gen.main)
-    # keyboard.add_hotkey("4", rock_paper_scissors.main)
-    # keyboard.add_hotkey("5", exit)
 
 
 def main():
@@ -20,15 +8,24 @@ def main():
         print("Python Projects")
         print(
             """
-0. Exit
 1. Dice Roll
 2. Mad Libs Generator
 3. Random Password Generator
 4. Rock Paper Scissors
+5. Exit
 """
         )
-        with Listener(on_press=check_key) as listener:
-            listener.join()
+        match input("Your selection: "):
+            case "1":
+                dice_roll.main()
+            case "2":
+                mad_libs_gen.main()
+            case "3":
+                rnd_password_gen.main()
+            case "4":
+                rock_paper_scissors.main()
+            case "5":
+                break
 
 
 if __name__ == "__main__":
